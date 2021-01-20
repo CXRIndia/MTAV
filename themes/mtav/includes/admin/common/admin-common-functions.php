f3b598552f73a9ef6f5a6b93955df9fe02e59563
vidyapawar95 <vpawar@cemtrexlabs.com>
2021-01-20 15:14:23 +0530

    Feat
    - File documentation

--- themes/mtav/includes/admin/common/admin-common-functions.php
+++ themes/mtav/includes/admin/common/admin-common-functions.php
@@ -1,2 +1,13 @@
 <?php
+/**
+ * Admin Common Functions
+ * php version 7.4
+ *
+ * @category MTAV
+ * @package  MTAV
+ * @author   Cemtrexlabs <hello@cemtrexlabs.com>
+ * @license  https://cemtrexlabs.com 1.0
+ * @link     MTAV
+ */
 
+// Silent is Gold
