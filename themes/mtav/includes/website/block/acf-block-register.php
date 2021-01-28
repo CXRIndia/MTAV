5f31a4cef87b353b997a64c4da131308d84d6847
vidyapawar95 <vpawar@cemtrexlabs.com>
2021-01-28 18:38:26 +0530

    Feat
    - Created settings for MTAV Players Block.

--- themes/mtav/includes/website/block/acf-block-register.php
+++ themes/mtav/includes/website/block/acf-block-register.php
@@ -151,6 +151,18 @@ function MTAV_Acf_init()
             )
         );
 
+        acf_register_block_type(
+            array(
+            'name'              => 'mtav-players-block',
+            'title'             => __('MTAV Players Block'),
+            'description'       => __('A custom players format block.'),
+            'render_callback'   => 'MTAV_Players_Block_Render_callback',
+            'category'          => 'mtav',
+            'icon'              => 'id-alt',
+            'keywords'          => array( 'image' ),
+            )
+        );
+
     }
 }
 
