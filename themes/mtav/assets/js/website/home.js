5031841c0be455bc2eb2baa35e304d874853a35c
swapniltoab <stawar@cemtrexlabs.com>
2021-01-29 14:31:31 +0530

    Added the Swiper and Scrollmagic

--- themes/mtav/assets/js/website/home.js
+++ themes/mtav/assets/js/website/home.js
@@ -1,5 +1,10 @@
 require('jquery');
 
+import TweenMax from "TweenMax";
+import ScrollMagic from 'ScrollMagic';
+import 'animation.gsap';
+import 'debug.addIndicators';
+
 require('../../scss/website/home-page.scss');
 (function ($) {
   console.log('Home js');
