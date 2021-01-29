2752e4707f05846bb470c2884cb98929c8b8af34
swapniltoab <stawar@cemtrexlabs.com>
2021-01-29 14:38:41 +0530

    Import swiper added in code

--- themes/mtav/assets/js/website/home.js
+++ themes/mtav/assets/js/website/home.js
@@ -5,6 +5,10 @@ import ScrollMagic from 'ScrollMagic';
 import 'animation.gsap';
 import 'debug.addIndicators';
 
+import Swiper from 'swiper';
+
+require('../../scss/website/components/mtav-swiper.scss');
+
 require('../../scss/website/home-page.scss');
 (function ($) {
   console.log('Home js');
