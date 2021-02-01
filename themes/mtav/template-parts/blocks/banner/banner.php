77a09a48453a652d4b5af6a2e77c89c6e655120c
Gajanannehete <gnehate@cemtrexlabs.com>
2021-02-01 10:52:36 +0530

    move banner section

--- themes/mtav/template-parts/blocks/banner/banner.php
+++ themes/mtav/template-parts/blocks/banner/banner.php
@@ -12,28 +12,3 @@
 
 ?>
 
-<div class="banner" style="background-image: url('<?php echo  esc_url(THEMEURI); ?>/assets/images/png/whoweare-banner.png')">
-    <div class="inner-wrapper">
-        <div class="container">
-            <div class="heading-compo">
-                <!-- <h5 class="title">
-                Who We Are
-                </h5> -->
-                <div class="line-block">
-                    <div class="left-block">
-                        <span></span>
-                        <span></span>
-                        <img src="<?php echo esc_url(THEMEURI); ?>/assets/images/icons/title-icon-wh.svg" alt="">
-                    </div>
-                    <div class="right-block">
-                        <span></span>
-                    </div>
-                </div>
-            </div>
-            <h1 class="brand-white">AN OPEN LETTER TO OUR FANS OF EVERY AGE, RACE AND GENDER</h1>
-            <p>Converting Sports Arenas into Voting Locations</p>
-            <button class="btn btn-primary">Meet the Team</button>
-        </div>
-    </div>
-</div>
-
