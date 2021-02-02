f2e4cc1ae2cc6e5b6b249567c0c35d57ef2f13bd
Gajanannehete <gnehate@cemtrexlabs.com>
2021-02-02 20:33:19 +0530

    404 page

--- themes/mtav/assets/js/website/error404.js
+++ themes/mtav/assets/js/website/error404.js
@@ -1,5 +1,7 @@
 require('jquery');
 
+require('../../scss/website/error404-page.scss');
+
 (function ($) {
 console.log('404 page js');
 })(jQuery);
--- themes/mtav/html-template/error404-page.php
+++ themes/mtav/html-template/error404-page.php
@@ -12,12 +12,29 @@
 get_header();
 ?>
 
-<div class="common-page error404-page">
+<div class="common-page error404-page header-position">
 
     <?php get_template_part('template-parts/content-header'); ?>
 
     <div class="body-content">
-
+        <div class="content-div">
+            <div class="container">
+                <div class="heading-compo black">
+                    <div class="line-block">
+                        <div class="left-block">
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </div>
+                </div>
+                <h1 class="main-title">
+                    NOT FOUND
+                </h1>
+                <p class="content">
+                    You just hit a route that doesn't exist... the sadness.
+                </p>
+            </div>
+        </div>
     </div>
 
     <?php get_template_part('template-parts/content-footer'); ?>
