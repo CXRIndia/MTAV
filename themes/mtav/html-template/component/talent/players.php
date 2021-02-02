b907bdf6c9adb0ea3506f9f68c2800709897dea1
Gajanannehete <gnehate@cemtrexlabs.com>
2021-02-02 12:28:46 +0530

    talent page

---
+++ themes/mtav/html-template/component/talent/players.php
@@ -0,0 +1,39 @@
+<div class="players-compo">
+    <div class="container">
+        <div class="header">
+            <div class="heading-compo black">
+                <div class="line-block">
+                    <div class="left-block">
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+            </div>
+
+            <h1 class="title">Players</h1>
+        </div>
+
+        <div class="players-wrapper">
+            <?php
+            for ($x = 0; $x <= 30; $x++) { ?>
+                <div class="single-block">
+                    <div>
+                        <div class="icon">
+                            <img src="<?php echo esc_url(THEMEURI); ?>/assets/images/icons/hand-icon.svg" alt="">
+                        </div>
+                        <div class="content-block">
+                            <h3 class="inner-title">
+                                ALESHIA OCASIO
+                            </h3>
+                            <a href="" target="_blank" class="link">
+                                @therealkendrickperkins
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            <?php }
+            ?>
+
+        </div>
+    </div>
+</div>
