8fe0d9729f2fdb1100bfd7598db68c2a0e37317c
komal <ksurandase@cemtrexlabs.com>
2021-02-03 17:05:30 +0530

    static content page design desktop

---
+++ themes/mtav/html-template/content-page.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Template Name: Content Page Template - HTML
+ * php version 7.4
+ *
+ * @category MTAV
+ * @package  MTAV
+ * @author   Cemtrexlabs <hello@cemtrexlabs.com>
+ * @license  https://cemtrexlabs.com 1.0
+ * @link     MTAV
+ */
+get_header();
+?>
+
+<div class="common-page campaigntemplate-page header-position">
+
+    <?php get_template_part('template-parts/content-header'); ?>
+
+    <div class="body-content content-main-wrapper">
+        <div class="container">
+            <h5>MORETHANAVOTE.ORG PRIVACY POLICY</h5>
+            <p>Effective Date: June 22nd, 2020</p>
+            <p>This privacy notice (this “Privacy Notice”) applies to any information that can reasonably be used to identify a natural person directly or indirectly (“Personal Information”) collected by More Than A Vote, Inc (“MTAV”, “we” or “us”) through websites, applications, or events operated by MTAV on behalf of itself and/or its subsidiaries (each, a “Site”), as well as any Personal Information provided to MTAV by any means.</p>
+            <p>Your privacy is important to us. We encourage you to read this Privacy Notice in its entirety before using our Sites or submitting Personal Information to us. If you choose to provide us with your Personal Information, you are telling us that you understand and accept the practices detailed in this Privacy Notice.</p>
+            <p>Our Sites may link to a limited number of other websites or applications. MTAV is not responsible for the practices of these third party websites and applications. We encourage you to read the privacy statements for each website you visit.</p>
+            <div class="spacer"></div>
+            <h5>WHEN DO WE COLLECT PERSONAL INFORMATION?</h5>
+            <p>We collect Personal Information:</p>
+            <ul>
+                <li>when you provide it to us</li>
+                <li>when you interact with us via social media</li>
+                <li>when you use our Sites</li>
+            </ul>
+            <p>Examples of the information we collect include:</p>
+            <p class="one-tab">If you sign one of our petitions, we will usually collect:</p>
+            <ul class="one-tab">
+                <li>Your full name, city and state</li>
+                <li>Any comments posted by you as public information</li>
+            </ul>
+            <p>What are cookies and how do we use them?</p>
+            <p class="one-tab">Our cookies fall into one the following categories or “types”:</p>
+            <ol class="one-tab">
+                <li>Strictly necessary – these are used to help make our Site work efficiently.</li>
+                <li>Performance – these are used to analyze the way our Site works and how we can improve it.</li>
+            </ol>
+        </div>
+
+    </div>
+
+    <?php get_template_part('template-parts/content-footer'); ?>
+
+ </div>
+
+<?php
+get_footer();
