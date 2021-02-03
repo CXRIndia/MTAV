04ead860afef76fc47b5e60d6873385911858dd2
vidyapawar95 <vpawar@cemtrexlabs.com>
2021-02-03 11:53:56 +0530

    Feat
    - Added custom settings for team post.

---
+++ themes/mtav/includes/website/acf-settings/options/mtav-team-settings.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * Custom team settings
+ * php version 7.4
+ *
+ * @category MTAV
+ * @package  MTAV
+ * @author   Cemtrexlabs <hello@cemtrexlabs.com>
+ * @license  https://cemtrexlabs.com 1.0
+ * @link     MTAV
+ */
+
+acf_add_local_field_group(
+    array(
+    'key' => 'group_601a38a5453e4',
+    'title' => 'MTAV Team Settings',
+    'fields' => array(
+        array(
+            'key' => 'field_601a39e1b2226',
+            'label' => 'Name',
+            'name' => 'name',
+            'type' => 'text',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'default_value' => '',
+            'placeholder' => '',
+            'prepend' => '',
+            'append' => '',
+            'maxlength' => '',
+        ),
+        array(
+            'key' => 'field_601a39ecb2227',
+            'label' => 'Instagram Feed',
+            'name' => 'instagram_feed',
+            'type' => 'text',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'default_value' => '',
+            'placeholder' => '',
+            'prepend' => '',
+            'append' => '',
+            'maxlength' => '',
+        ),
+        array(
+            'key' => 'field_601a3a00b2228',
+            'label' => 'Job Title',
+            'name' => 'job_title',
+            'type' => 'text',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'default_value' => '',
+            'placeholder' => '',
+            'prepend' => '',
+            'append' => '',
+            'maxlength' => '',
+        ),
+        array(
+            'key' => 'field_601a3a17b2229',
+            'label' => 'Person Image',
+            'name' => 'person_image',
+            'type' => 'image',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'return_format' => 'id',
+            'preview_size' => 'medium',
+            'library' => 'all',
+            'min_width' => '',
+            'min_height' => '',
+            'min_size' => '',
+            'max_width' => '',
+            'max_height' => '',
+            'max_size' => '',
+            'mime_types' => '',
+        ),
+        array(
+            'key' => 'field_601a410010315',
+            'label' => 'Select Person Designation',
+            'name' => 'select_person_designation',
+            'type' => 'radio',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'choices' => array(
+                'player' => 'Player',
+                'staff' => 'Staff',
+            ),
+            'allow_null' => 0,
+            'other_choice' => 0,
+            'default_value' => '',
+            'layout' => 'horizontal',
+            'return_format' => 'value',
+            'save_other_choice' => 0,
+        ),
+    ),
+    'location' => array(
+        array(
+            array(
+                'param' => 'post_type',
+                'operator' => '==',
+                'value' => 'team',
+            ),
+        ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => true,
+    'description' => '',
+    )
+);
