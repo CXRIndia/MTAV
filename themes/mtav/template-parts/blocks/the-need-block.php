5d0f43e3a74a6964b085f2b1a3571defe50c7626
vidyapawar95 <vpawar@cemtrexlabs.com>
2021-02-03 16:12:38 +0530

    Feat
    - Integrated the need section on campaign page.

---
+++ themes/mtav/template-parts/blocks/the-need-block.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * The Need block template.
+ * php version 7.4
+ *
+ * @category MTAV
+ * @package  MTAV
+ * @author   Cemtrexlabs <hello@cemtrexlabs.com>
+ * @license  https://cemtrexlabs.com 1.0
+ * @link     MTAV
+ */
+
+?>
+
+<?php if($block_data && !empty($block_data)) :?>
+<div class="need-solve--wrapper">
+
+        <div class="container">
+
+            <div class="need-solve--inner-wrapper">
+
+            <?php foreach ( $block_data as $key=>$data) {
+
+                $title            = $data['title'];
+                $description      = $data['description_data'];
+                $block_img_id     = $data['image'];
+                $block_img_array  = wp_get_attachment_image_src($block_img_id, 'full');
+                $block_img_alt    = MTAV_Get_Image_alt($block_img_id, "Block Image");
+                $block_img_url    = MTAV_Get_image($block_img_array);
+
+                if ($key % 2 == 0) {?>
+                <div class="need-solve--left">
+                    <div class="need-solve--content">
+                        <div>
+                            <div class="heading-compo black">
+                                <div class="line-block">
+                                    <div class="left-block">
+                                        <span></span>
+                                        <span></span>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <?php if($title && !empty($title)) :?>
+                                <h1><?php echo wp_kses_post($title);?></h1>
+                            <?php endif;?>
+
+                        </div>
+
+                        <?php if($description && !empty($description)) :?>
+                        <div class="content-para">
+                             <?php echo wp_kses_post(MTAV_Remove_ptag($description));?>
+                        </div>
+                        <?php endif;?>
+
+                    </div>
+
+                    <?php if($block_img_id && !empty($block_img_id)) :?>
+                    <div class="need-solve--image">
+                        <img src="<?php echo esc_url($block_img_url); ?>" />
+                    </div>
+                    <?php endif;?>
+
+                </div>
+                <?php } else {?>
+
+                <div class="need-solve--right">
+
+                    <?php if($block_img_id && !empty($block_img_id)) :?>
+                    <div class="need-solve--image">
+                        <img src="<?php echo esc_url($block_img_url); ?>" />
+                    </div>
+                    <?php endif;?>
+
+                    <div class="need-solve--content">
+                        <div>
+                            <div class="heading-compo black">
+                                <div class="line-block">
+                                    <div class="left-block">
+                                        <span></span>
+                                        <span></span>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <?php if($title && !empty($title)) :?>
+                                <h1><?php echo wp_kses_post($title);?></h1>
+                            <?php endif;?>
+
+                        </div>
+
+                        <?php if($description && !empty($description)) :?>
+                        <div class="content-para">
+                             <?php echo wp_kses_post(MTAV_Remove_ptag($description));?>
+                        </div>
+                        <?php endif;?>
+
+                    </div>
+                </div>
+                <?php }
+            }?>
+            </div>
+        </div>
+
+</div>
+
+<?php endif; ?>
+
+
