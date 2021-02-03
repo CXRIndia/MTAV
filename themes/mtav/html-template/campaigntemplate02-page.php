ec25e3b00e4e410f8a22185cd5e2c4da300ee891
komal <ksurandase@cemtrexlabs.com>
2021-02-03 14:35:29 +0530

    campaign template page design

---
+++ themes/mtav/html-template/campaigntemplate02-page.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Template Name: Campaign Template 02 - HTML
+ * php version 7.4
+ *
+ * @category MTAV
+ * @package  MTAV
+ * @author   Cemtrexlabs <hello@cemtrexlabs.com>
+ * @license  https://cemtrexlabs.com 1.0
+ * @link     MTAV
+ */
+get_header();
+?>
+
+<div class="common-page campaigntemplate-page header-position">
+
+    <?php get_template_part('template-parts/content-header'); ?>
+
+    <div class="body-content campaigntemplate-main-wrapper">
+        <?php require THEMEPATH . '/html-template/component/campaigntemplate/contactinformation.php'; ?>
+        <?php require THEMEPATH . '/html-template/component/campaign-launch.php'; ?>
+    </div>
+
+    <?php get_template_part('template-parts/content-footer'); ?>
+
+ </div>
+
+<?php
+get_footer();
