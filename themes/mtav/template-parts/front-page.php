e8bb1689bf6893c1392fc34eb91e30085a919259
vidyapawar95 <vpawar@cemtrexlabs.com>
2021-02-03 20:55:35 +0530

    Feat
    - Added latest home banner.

--- themes/mtav/template-parts/front-page.php
+++ themes/mtav/template-parts/front-page.php
@@ -1,17 +1,3 @@
-<?php
-/**
- * Front page template file
- * php version 7.4
-
- * @category MTAV
- * @package  MTAV
- * @author   Cemtrexlabs <hello@cemtrexlabs.com>
- * @license  https://cemtrexlabs.com 1.0
- * @link     MTAV
- */
-
-?>
-
 <div class="js-hand-trigger"></div>
 <div class="dummy"></div>
 <div class="hand-banner js-handanimation">
@@ -30,26 +16,68 @@
                 <img src="<?php echo esc_url(THEMEURI); ?>/assets/images/icons/scrolldown-arrow.svg" alt="">
             </div>
 
-            <div class="hand1-wrapper">
-                <img src="<?php echo esc_url(THEMEURI); ?>/assets/images/hands/hand-1.png" alt="">
+            <div class="dsk-block">
+                <div class="hand1-wrapper">
+                    <img src="<?php echo esc_url(THEMEURI); ?>/assets/images/hands/hand-1.png" alt="">
+                </div>
+                <div class="hand2-wrapper">
+                    <img src="<?php echo esc_url(THEMEURI); ?>/assets/images/hands/hand-2.png" alt="">
+                </div>
+                <div class="hand3-wrapper">
+                    <img src="<?php echo esc_url(THEMEURI); ?>/assets/images/hands/hand-3.png" alt="">
+                </div>
+                <div class="hand4-wrapper">
+                    <img src="<?php echo esc_url(THEMEURI); ?>/assets/images/hands/hand-4.png" alt="">
+                </div>
+                <div class="hand5-wrapper">
+                    <img src="<?php echo esc_url(THEMEURI); ?>/assets/images/hands/hand-5.png" alt="">
+                </div>
+                <div class="hand6-wrapper">
+                    <img src="<?php echo esc_url(THEMEURI); ?>/assets/images/hands/hand-6.png" alt="">
+                </div>
             </div>
-            <div class="hand2-wrapper">
-                <img src="<?php echo esc_url(THEMEURI); ?>/assets/images/hands/hand-2.png" alt="">
+
+            <div class="tablet-block">
+                <div class="hand1-wrapper">
+                    <img src="<?php echo esc_url(THEMEURI); ?>/assets/images/hands/tablet/tablet-hand-1.png" alt="">
+                </div>
+                <div class="hand2-wrapper">
+                    <img src="<?php echo esc_url(THEMEURI); ?>/assets/images/hands/tablet/tablet-hand-2.png" alt="">
+                </div>
+                <div class="hand3-wrapper">
+                    <img src="<?php echo esc_url(THEMEURI); ?>/assets/images/hands/tablet/tablet-hand-3.png" alt="">
+                </div>
+                <div class="hand4-wrapper">
+                    <img src="<?php echo esc_url(THEMEURI); ?>/assets/images/hands/tablet/tablet-hand-4.png" alt="">
+                </div>
+                <div class="hand5-wrapper">
+                    <img src="<?php echo esc_url(THEMEURI); ?>/assets/images/hands/tablet/tablet-hand-5.png" alt="">
+                </div>
+                <div class="hand6-wrapper">
+                    <img src="<?php echo esc_url(THEMEURI); ?>/assets/images/hands/tablet/tablet-hand-6.png" alt="">
+                </div>
             </div>
-            <div class="hand3-wrapper">
-                <img src="<?php echo esc_url(THEMEURI); ?>/assets/images/hands/hand-3.png" alt="">
-            </div>
-            <div class="hand4-wrapper">
-                <img src="<?php echo esc_url(THEMEURI); ?>/assets/images/hands/hand-4.png" alt="">
-            </div>
-            <div class="hand5-wrapper">
-                <img src="<?php echo esc_url(THEMEURI); ?>/assets/images/hands/hand-5.png" alt="">
-            </div>
-            <div class="hand6-wrapper">
-                <img src="<?php echo esc_url(THEMEURI); ?>/assets/images/hands/hand-6.png" alt="">
+
+            <div class="mo-block">
+                <div class="hand1-wrapper">
+                    <img src="<?php echo esc_url(THEMEURI); ?>/assets/images/hands/mobile/mobile-hand-1.png" alt="">
+                </div>
+                <div class="hand2-wrapper">
+                    <img src="<?php echo esc_url(THEMEURI); ?>/assets/images/hands/mobile/mobile-hand-2.png" alt="">
+                </div>
+                <div class="hand3-wrapper">
+                    <img src="<?php echo esc_url(THEMEURI); ?>/assets/images/hands/mobile/mobile-hand-3.png" alt="">
+                </div>
+                <div class="hand4-wrapper">
+                    <img src="<?php echo esc_url(THEMEURI); ?>/assets/images/hands/mobile/mobile-hand-4.png" alt="">
+                </div>
+                <div class="hand5-wrapper">
+                    <img src="<?php echo esc_url(THEMEURI); ?>/assets/images/hands/mobile/mobile-hand-5.png" alt="">
+                </div>
+                <div class="hand6-wrapper">
+                    <img src="<?php echo esc_url(THEMEURI); ?>/assets/images/hands/mobile/mobile-hand-6.png" alt="">
+                </div>
             </div>
         </div>
     </div>
 </div>
-
-<?php the_content(); ?>
