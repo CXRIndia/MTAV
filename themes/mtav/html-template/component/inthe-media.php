f59b7a95bfb37958fe36cd2d37ea72b61c42a9c3
Tushar F <tfirake@cemtrexlabs.com>
2021-02-03 15:55:38 +0530

    Feat - Campign In the Media slider implemented

---
+++ themes/mtav/html-template/component/inthe-media.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * In the Media template
+ * php version 7.4
+ *
+ * @category MTAV
+ * @package  MTAV
+ * @author   Cemtrexlabs <hello@cemtrexlabs.com>
+ * @license  https://cemtrexlabs.com 1.0
+ * @link     MTAV
+ */
+
+?>
+
+<div class="the-media--wrapper">
+    <div class="container">
+        <div>
+            <div class="heading-compo black">
+                <div class="line-block">
+                    <div class="left-block">
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+            </div>
+            <h1>in the media</h1>
+        </div>
+    </div>
+
+    <div class="media-slider-wrapper">
+            <div class="swiper-container media-slider">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide" style="background-image: url(<?php echo esc_url(THEMEURI); ?>/assets/images/the-need.jpg);">
+                        <div class="slider-content">
+                            <div class="title">Lebron james joins push to turn out ex-felon vote in florida</div>
+                            <a href="#" class="btn btn-primary">Read the full story</a>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide" style="background-image: url(<?php echo esc_url(THEMEURI); ?>/assets/images/the-need.jpg);">
+                        <div class="slider-content">
+                            <div class="title">Lebron james joins push to turn out ex-felon vote in florida</div>
+                            <a href="#" class="btn btn-primary">Read the full story</a>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide" style="background-image: url(<?php echo esc_url(THEMEURI); ?>/assets/images/the-need.jpg);">
+                        <div class="slider-content">
+                            <div class="title">Lebron james joins push to turn out ex-felon vote in florida</div>
+                            <a href="#" class="btn btn-primary">Read the full story</a>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide" style="background-image: url(<?php echo esc_url(THEMEURI); ?>/assets/images/the-need.jpg);">
+                        <div class="slider-content">
+                            <div class="title">Lebron james joins push to turn out ex-felon vote in florida</div>
+                            <a href="#" class="btn btn-primary">Read the full story</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="media-slider--buttons">
+                <!-- Add Arrows -->
+                <div class="swiper-button-next media-next" style="background-image: url(<?php echo esc_url(THEMEURI); ?>/assets/images/icons/media-slider-next.svg);"></div>
+                <div class="swiper-button-prev media-prev" style="background-image: url(<?php echo esc_url(THEMEURI); ?>/assets/images/icons/media-slider-prev.svg);"></div>
+            </div>
+        </div>
+</div>
