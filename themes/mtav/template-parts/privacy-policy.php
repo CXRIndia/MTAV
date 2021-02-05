ddc215c15739ba3c0bce16618dfea5806ec42881
vidyapawar95 <vpawar@cemtrexlabs.com>
2021-02-05 12:36:28 +0530

    Feat
    - Integrated privacy policy page.

---
+++ themes/mtav/template-parts/privacy-policy.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * Privacy Policy template
+ * php version 7.4
+ *
+ * @category MTAV
+ * @package  MTAV
+ * @author   Cemtrexlabs <hello@cemtrexlabs.com>
+ * @license  https://cemtrexlabs.com 1.0
+ * @link     MTAV
+ */
+
+?>
+
+<div class="container">
+    <h5>MORETHANAVOTE.ORG PRIVACY POLICY</h5>
+    <p>Effective Date: June 22nd, 2020</p>
+    <p>This privacy notice (this “Privacy Notice”) applies to any information that can reasonably be used to identify a natural person directly or indirectly (“Personal Information”) collected by More Than A Vote, Inc (“MTAV”, “we” or “us”) through websites, applications, or events operated by MTAV on behalf of itself and/or its subsidiaries (each, a “Site”), as well as any Personal Information provided to MTAV by any means.</p>
+    <p>Your privacy is important to us. We encourage you to read this Privacy Notice in its entirety before using our Sites or submitting Personal Information to us. If you choose to provide us with your Personal Information, you are telling us that you understand and accept the practices detailed in this Privacy Notice.</p>
+    <p>Our Sites may link to a limited number of other websites or applications. MTAV is not responsible for the practices of these third party websites and applications. We encourage you to read the privacy statements for each website you visit.</p>
+    <div class="spacer"></div>
+    <h5>WHEN DO WE COLLECT PERSONAL INFORMATION?</h5>
+    <p>We collect Personal Information:</p>
+    <ul>
+        <li>when you provide it to us</li>
+        <li>when you interact with us via social media</li>
+        <li>when you use our Sites</li>
+    </ul>
+    <p>Information you provide to us:</p>
+    <p>You may give us Personal Information when:</p>
+    <ul class="one-tab">
+        <li>you use our Sites, or interact with us or our representatives</li>
+        <li>you donate to us</li>
+        <li>you sign up for or participate in one of our events</li>
+        <li>we communicate with each other</li>
+        <li>you sign up for our mailing list or newsletters</li>
+        <li>you order products from us and/or have them shipped to you or others</li>
+        <li>we facilitate your registration to vote</li>
+        <li>you sign one of our petitions</li>
+        <li>you leave a comment on one of our Sites or social media feeds.</li>
+    </ul>
+    <p>Social Media:</p>
+    <p class="one-tab">We may obtain information about you from your social media accounts or services when you choose to interact with us 	(for example, when you 	log in to leave a comment on a social media platform such as Facebook, Instagram, TikTok, Snapchat and Twitter.) Please check your settings and the privacy policies of the social media sites you use for more information.</p>
+    <p class="one-tab">We also use “cookies” to help us improve the performance of our Sites and campaigns. You can find out more about our use of cookies in our cookie policy.</p>
+    <div class="spacer"></div>
+    <h5>WHAT INFORMATION DO WE COLLECT?</h5>
+    <p>We collect Personal Information that helps us make informed decisions about the resources we provide and manage, fundraise more efficiently, and give you the best possible experience during our events, on our website, and from our communications</p>
+    <p>If we facilitate your registration to vote (typically by re-directing you to other sites through links included in our Sites), you will be providing the information required in your State to process your registration to one of our partner ogranizations or directly to a governmental entity responsible for processing voter registration.</p>
+    <p>Examples of the information we collect include:</p>
+    <p class="one-tab">If you sign one of our petitions, we will usually collect:</p>
+    <ul class="one-tab">
+        <li>Your full name, city and state</li>
+        <li>Any comments posted by you as public information</li>
+    </ul>
+    <p class="one-tab">If you volunteer with us or sign-up for our events, we will usually collect your:</p>
+    <ul class="one-tab">
+        <li>full name</li>
+        <li>email address</li>
+        <li>telephone number</li>
+        <li>city, state/province, country</li>
+    </ul>
+    <p class="one-tab">If you purchase a product from us, we will usually collect:</p>
+    <ul class="one-tab">
+        <li>full name</li>
+        <li>contact information (e.g. email address, telephone number)</li>
+        <li>shipment information</li>
+        <li>payment information</li>
+    </ul>
+    <p class="one-tab">When you use our Siteswe automatically collect Usage Information which may include:</p>
+    <ul class="one-tab">
+        <li>your IP address and other unique identifiers</li>
+        <li>information about your browser and operating system</li>
+        <li>referring and exit web pages and URLs</li>
+        <li>time and day you visit our Sites</li>
+        <li>pages of our Sites that you visit</li>
+        <li>your device characteristics</li>
+    </ul>
+    <div class="spacer"></div>
+    <h5>HOW DO WE USE YOUR PERSONAL INFORMATION?</h5>
+    <p>In general, we use your Personal Information,to:</p>
+    <ul class="one-tab">
+        <li>send you updates and other communications</li>
+        <li>answer your enquiries</li>
+        <li>keep a record of your engagement with us</li>
+        <li>improve our services, provide technical support, address problems with our Sites and protect their security and integrity</li>
+        <li>comply with legal requirements that apply to us</li>
+    </ul>
+    <p>We may use direct communications to let you know what MTAV is doing and how your support makes a difference. We will always respect your preferences and endeavor to send you information that you might find interesting, in the format you prefer. If you no longer wish to receive direct communications from us:</p>
+    <ul class="one-tab">
+        <li>For e-mail communication, you can update your preferences anytime or opt out by using the unsubscribe link in the footer of each email.</li>
+        <li>We may use your cell phone number to provide you with periodic automated text messages and calls. Message & data rates may apply. Text STOP to cancel or HELP for help.</li>
+        <li>If you sign one of our petitions, we may use your information to provide compilations of petitions, with your comments 	included, to national leaders, without disclosing your e-mail addresses. We may also make comments along with your city and state available to the press and public online.</li>
+    </ul>
+    <div class="spacer"></div>
+    <h5>WHO HAS ACCESS TO YOUR PERSONAL INFORMATION?</h5>
+    <p>Trained Staff</p>
+    <p class="one-tab">The Personal Information we hold about you is only accessible by trained staff, volunteers, and contractors. We regularly review who has access to Personal Information and do comprehensive checks on any contractors before we work with them.<p>
+    <p>Vendors</p>
+    <p class="one-tab">We use other companies to help us manage and store personal data, and to carry out certain activities on our behalf. These may include service providers that (a) conduct research and analytics; (b) create content; (c) host our Sites; (d) handle donations; (e) maintain our databases, or (f) otherwise provide support to us.</p>
+    <p>Partner organizations</p>
+    <p class="one-tab">We may share your contact information with other like-minded organizations such as Rock the Vote, and they may contact you or solicit from you.</p>
+    <p class="one-tab">However, we will not:</p>
+    <ul class="one-tab">
+        <li>Sell or exchange under any circumstances your payment information with any organization other than the service providers that help us process your donations or payments.</li>
+        <li>Share text messaging opt-in data and/or consent (that is to say, we will only text you if you have opted-in to receive texts from us and we will not authorize partner organizations to text you unless you opt-in to receive texts from them)</li>
+    </ul>
+    <p>Other</p>
+    <p class="one-tab">We may be required to disclose your Personal Information to governmental authorities or to third parties pursuant to law enforcement inquiries, court orders, subpoenas and similar requests. We may also share when we otherwise believe in good faith that such disclosure is necessary or appropriate in connection with any activity that may violate the law; is contrary to our policies, or may expose us or others to liability.</p>
+    <div class="spacer"></div>
+    <h5>HOW DO WE KEEP PERSONAL INFORMATION ABOUT YOU SAFE?</h5>
+    <p>We retain Personal Information about you only for as long as necessary for the purposes for which the data was collected, except where necessary to meet our legal obligations or in order to establish, exercise, defend potential legal claims.</p>
+    <p>If you request to receive no further communications from us, we will keep some basic information about you on our suppression lists in order to avoid sending you unwanted communications in the future.</p>
+    <div class="spacer"></div>
+    <h5>INFORMATION ON CHILDREN</h5>
+    <p>We do not knowingly collect personal information from children under 13. Our Sites are not intended to solicit information of any kind from children under 13, and we have designed them to not knowing accept information from children under 13 whenever age-related information is requested.</p>
+    <div class="spacer"></div>
+    <h5>CHANGES TO THIS PRIVACY NOTICE</h5>
+    <p>MTAV will occasionally update this Privacy Notice as changes to our Sites or practices necessitate it. If we make any significant changes in the way we treat your Personal Information, we will make this clear on our websites or by contacting you directly.</p>
+    <div class="spacer"></div>
+    <h5>HOW TO CONTACT US</h5>
+    <p>This Privacy Notice was prepared to be as comprehensive as possible, but it does not include an exhaustive list of every aspect of our collection and use of Personal Information. However, we would be happy to provide any further information or explanation about our practices.</p>
+    <p>If you have any questions, comments or suggestions, please let us know by contacting us at legal@morethanavote.org.</p>
+</div>
