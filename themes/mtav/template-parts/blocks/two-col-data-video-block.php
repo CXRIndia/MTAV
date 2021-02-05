6742bc01e178244a46fd1bf73480d38f5af6e98b
vidyapawar95 <vpawar@cemtrexlabs.com>
2021-02-05 18:41:48 +0530

    Fix
    - Added p tag.

--- themes/mtav/template-parts/blocks/two-col-data-video-block.php
+++ themes/mtav/template-parts/blocks/two-col-data-video-block.php
@@ -59,7 +59,7 @@
 
                 <?php if($description && !empty($description)) :?>
                 <div class="para">
-                    <?php echo wp_kses_post(MTAV_Remove_ptag($description));?>
+                    <?php echo wp_kses_post($description);?>
                 </div>
                 <?php endif; ?>
 
