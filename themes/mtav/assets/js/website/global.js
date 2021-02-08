0b1283b7aa06945cc8c0693fda888ab0aa348099
swapniltoab <stawar@cemtrexlabs.com>
2021-02-08 15:58:17 +0530

    AOS package added

--- themes/mtav/assets/js/website/global.js
+++ themes/mtav/assets/js/website/global.js
@@ -3,7 +3,6 @@ require('jquery');
 import 'lazysizes';
 
 import AOS from 'aos';
-import 'aos/dist/aos.css';
 
 window.lazySizesConfig = window.lazySizesConfig || {};
 window.lazySizesConfig.loadMode = 1;
@@ -40,29 +39,29 @@ window.lazySizesConfig.loadMode = 1;
 
   $(window).load(function () {
 
-    var wpcf7Elm = document.querySelector('.wpcf7');
-    wpcf7Elm.addEventListener('wpcf7mailsent', function (event) {
-      $('#contact_hide').hide()
-      $("#contact_form_msg").text("Mail Sent Successfully!");
-      setTimeout(function () {
-        $('#contact_hide').show();
-        $("#contact_form_msg").hide();
-      }, 7000);
-    }, false);
+    // var wpcf7Elm = document.querySelector('.wpcf7');
+    // wpcf7Elm.addEventListener('wpcf7mailsent', function (event) {
+    //   $('#contact_hide').hide()
+    //   $("#contact_form_msg").text("Mail Sent Successfully!");
+    //   setTimeout(function () {
+    //     $('#contact_hide').show();
+    //     $("#contact_form_msg").hide();
+    //   }, 7000);
+    // }, false);
 
-    wpcf7Elm.addEventListener('wpcf7mailfailed', function (event) {
-      $('#contact_hide').hide()
-      $("#contact_form_msg").text("Something went Wrong. Please try again!");
-      setTimeout(function () {
-        $('#contact_hide').show();
-        $("#contact_form_msg").hide();
-      }, 7000);
-    }, false);
+    // wpcf7Elm.addEventListener('wpcf7mailfailed', function (event) {
+    //   $('#contact_hide').hide()
+    //   $("#contact_form_msg").text("Something went Wrong. Please try again!");
+    //   setTimeout(function () {
+    //     $('#contact_hide').show();
+    //     $("#contact_form_msg").hide();
+    //   }, 7000);
+    // }, false);
   });
 
-  $(window).scroll(function(){
+  $(window).scroll(function () {
     var sticky = $('header'),
-        scroll = $(window).scrollTop();
+      scroll = $(window).scrollTop();
 
     if (scroll >= 50) sticky.addClass('fixed');
     else sticky.removeClass('fixed');
