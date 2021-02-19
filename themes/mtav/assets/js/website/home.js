f1eff48b8da64d64f0744c4fae2b3c5779e7760b
Tushar F <tfirake@cemtrexlabs.com>
2021-02-19 15:59:22 +0530

    Fix - Hand animation safari glitch fixed

--- themes/mtav/assets/js/website/home.js
+++ themes/mtav/assets/js/website/home.js
@@ -1,21 +1,21 @@
 import "TweenMax";
-import ScrollMagic from 'ScrollMagic';
-import 'animation.gsap';
-import 'debug.addIndicators';
+import ScrollMagic from "ScrollMagic";
+import "animation.gsap";
+import "debug.addIndicators";
 
-import Swiper, { Navigation } from 'swiper';
+import Swiper, { Navigation } from "swiper";
 // configure Swiper to use modules
 Swiper.use([Navigation]);
 
-require('../../scss/website/components/mtav-swiper.scss');
+require("../../scss/website/components/mtav-swiper.scss");
 
-require('../../scss/website/home-page.scss');
+require("../../scss/website/home-page.scss");
 
 (function ($) {
-  console.log('Home js');
+  console.log("Home js");
 
   $(window).load(function () {
-    $('#overlay').fadeOut();
+    $("#overlay").fadeOut();
   });
 
   var p = $(".imgvideo-title-sec");
@@ -23,23 +23,22 @@ require('../../scss/website/home-page.scss');
   offset = offset.top;
 
   $(window).scroll(function () {
-
     var sc = $(window).scrollTop();
     // console.log("Scroll", sc);
     // console.log("window offset", offset)
 
     if ($(window).scrollTop() > offset + 900) {
-      $('header').fadeIn();
+      $("header").fadeIn();
     } else {
-      $('header').fadeOut();
+      $("header").fadeOut();
     }
   });
 
   // START : Home By The NUmber Js
-  var swiper = new Swiper('.bythenumber-slider', {
+  var swiper = new Swiper(".bythenumber-slider", {
     navigation: {
-      nextEl: '.bythenumber-next',
-      prevEl: '.bythenumber-prev',
+      nextEl: ".bythenumber-next",
+      prevEl: ".bythenumber-prev",
     },
   });
   // END : Home By The NUmber Js
@@ -61,276 +60,248 @@ require('../../scss/website/home-page.scss');
   let wwidth = $(window).width();
   console.log(wwidth);
   if (wwidth > 1024) {
-    console.log('Hand Animation Desktop');
+    console.log("Hand Animation Desktop");
     //Timeinterval for animation delay
+    wipeAnimation.add([TweenMax.to(".dummy", 0.2, {})]);
     wipeAnimation.add([
-      TweenMax.to(".dummy", .2, {})
-    ]);
-    wipeAnimation.add([
-      TweenMax.to(".hand1-wrapper", .5, {
-        left: '-40%',
+      TweenMax.to(".hand1-wrapper", 0.5, {
+        left: "-40%",
         ease: Linear.easeNone,
       }),
-      TweenMax.to(".hand2-wrapper", .5, {
-        right: '-35%',
+      TweenMax.to(".hand2-wrapper", 0.5, {
+        right: "-35%",
         ease: Linear.easeNone,
       }),
     ]);
 
     //Timeinterval for animation delay
+    wipeAnimation.add([TweenMax.to(".dummy", 0.2, {})]);
     wipeAnimation.add([
-      TweenMax.to(".dummy", .2, {})
-    ]);
-    wipeAnimation.add([
-      TweenMax.to(".hand3-wrapper", .5, {
-        top: '-29%',
-        right: '5%',
+      TweenMax.to(".hand3-wrapper", 0.5, {
+        top: "-29%",
+        right: "5%",
         ease: Linear.easeNone,
       }),
-      TweenMax.to(".hand4-wrapper", .5, {
-        bottom: '-32%',
-        left: '9%',
+      TweenMax.to(".hand4-wrapper", 0.5, {
+        bottom: "-32%",
+        left: "9%",
         ease: Linear.easeNone,
       }),
     ]);
 
     //Timeinterval for animation delay
+    wipeAnimation.add([TweenMax.to(".dummy", 0.2, {})]);
     wipeAnimation.add([
-      TweenMax.to(".dummy", .2, {})
-    ]);
-    wipeAnimation.add([
-      TweenMax.to(".hand5-wrapper", .5, {
-        bottom: '-36%',
-        right: '4%',
+      TweenMax.to(".hand5-wrapper", 0.5, {
+        bottom: "-36%",
+        right: "4%",
         ease: Linear.easeNone,
       }),
-      TweenMax.to(".hand6-wrapper", .5, {
-        top: '-33%',
-        left: '4%',
+      TweenMax.to(".hand6-wrapper", 0.5, {
+        top: "-33%",
+        left: "4%",
         ease: Linear.easeNone,
       }),
       TweenMax.to(".logo-wrapper--anim", 0.3, {
-        marginTop: '0',
-        opacity: '1',
+        marginTop: "0",
+        opacity: "1",
         ease: Linear.easeNone,
       }),
     ]);
 
     //Timeinterval for animation delay
+    wipeAnimation.add([TweenMax.to(".dummy", 0.2, {})]);
     wipeAnimation.add([
-      TweenMax.to(".dummy", .2, {})
-    ]);
-    wipeAnimation.add([
-      TweenMax.to(".hand1-wrapper", .3, {
-        left: '-60%',
+      TweenMax.to(".hand1-wrapper", 0.3, {
+        left: "-60%",
         ease: Linear.easeNone,
       }),
-      TweenMax.to(".hand2-wrapper", .3, {
-        right: '-60%',
+      TweenMax.to(".hand2-wrapper", 0.3, {
+        right: "-60%",
         ease: Linear.easeNone,
       }),
-      TweenMax.to(".hand3-wrapper", .3, {
-        top: '-60%',
+      TweenMax.to(".hand3-wrapper", 0.3, {
+        top: "-60%",
         ease: Linear.easeNone,
       }),
-      TweenMax.to(".hand4-wrapper", .3, {
-        bottom: '-65%',
+      TweenMax.to(".hand4-wrapper", 0.3, {
+        bottom: "-70%",
         ease: Linear.easeNone,
       }),
-      TweenMax.to(".hand5-wrapper", .3, {
-        bottom: '-70%',
+      TweenMax.to(".hand5-wrapper", 0.3, {
+        bottom: "-75%",
         ease: Linear.easeNone,
       }),
-      TweenMax.to(".hand6-wrapper", .3, {
-        top: '-60%',
+      TweenMax.to(".hand6-wrapper", 0.3, {
+        top: "-60%",
         ease: Linear.easeNone,
       }),
-      TweenMax.to(".scroll-arrow", .3, {
-        bottom: '-80px',
+      TweenMax.to(".scroll-arrow", 0.3, {
+        bottom: "-80px",
         ease: Linear.easeNone,
       }),
     ]);
-  }
-
-  else if (wwidth > 767) {
-    console.log('Hand Animation Tablet');
+  } else if (wwidth > 767) {
+    console.log("Hand Animation Tablet");
     //Timeinterval for animation delay
+    wipeAnimation.add([TweenMax.to(".dummy", 0.2, {})]);
     wipeAnimation.add([
-      TweenMax.to(".dummy", .2, {})
-    ]);
-    wipeAnimation.add([
-      TweenMax.to(".hand1-wrapper", .5, {
-        left: '-42%',
+      TweenMax.to(".hand1-wrapper", 0.5, {
+        left: "-42%",
         ease: Linear.easeNone,
       }),
-      TweenMax.to(".hand2-wrapper", .5, {
-        right: '-44%',
+      TweenMax.to(".hand2-wrapper", 0.5, {
+        right: "-44%",
         ease: Linear.easeNone,
       }),
     ]);
 
     //Timeinterval for animation delay
+    wipeAnimation.add([TweenMax.to(".dummy", 0.2, {})]);
     wipeAnimation.add([
-      TweenMax.to(".dummy", .2, {})
-    ]);
-    wipeAnimation.add([
-      TweenMax.to(".hand3-wrapper", .5, {
-        top: '-28%',
-        right: '-41%',
+      TweenMax.to(".hand3-wrapper", 0.5, {
+        top: "-28%",
+        right: "-41%",
         ease: Linear.easeNone,
       }),
-      TweenMax.to(".hand4-wrapper", .5, {
-        bottom: '-32%',
-        left: '-26%',
+      TweenMax.to(".hand4-wrapper", 0.5, {
+        bottom: "-32%",
+        left: "-26%",
         ease: Linear.easeNone,
       }),
     ]);
 
     //Timeinterval for animation delay
+    wipeAnimation.add([TweenMax.to(".dummy", 0.2, {})]);
     wipeAnimation.add([
-      TweenMax.to(".dummy", .2, {})
-    ]);
-    wipeAnimation.add([
-      TweenMax.to(".hand5-wrapper", .5, {
-        bottom: '-41%',
-        right: '-31%',
+      TweenMax.to(".hand5-wrapper", 0.5, {
+        bottom: "-41%",
+        right: "-31%",
         ease: Linear.easeNone,
       }),
-      TweenMax.to(".hand6-wrapper", .5, {
-        top: '-34%',
-        left: '-30%',
+      TweenMax.to(".hand6-wrapper", 0.5, {
+        top: "-34%",
+        left: "-30%",
         ease: Linear.easeNone,
       }),
       TweenMax.to(".logo-wrapper--anim", 0.3, {
-        marginTop: '0',
-        opacity: '1',
+        marginTop: "0",
+        opacity: "1",
         ease: Linear.easeNone,
       }),
     ]);
 
     //Timeinterval for animation delay
+    wipeAnimation.add([TweenMax.to(".dummy", 0.2, {})]);
     wipeAnimation.add([
-      TweenMax.to(".dummy", .2, {})
-    ]);
-    wipeAnimation.add([
-      TweenMax.to(".hand1-wrapper", .3, {
-        left: '-60%',
+      TweenMax.to(".hand1-wrapper", 0.3, {
+        left: "-60%",
         ease: Linear.easeNone,
       }),
-      TweenMax.to(".hand2-wrapper", .3, {
-        right: '-60%',
+      TweenMax.to(".hand2-wrapper", 0.3, {
+        right: "-60%",
         ease: Linear.easeNone,
       }),
-      TweenMax.to(".hand3-wrapper", .3, {
-        top: '-60%',
+      TweenMax.to(".hand3-wrapper", 0.3, {
+        top: "-60%",
         ease: Linear.easeNone,
       }),
-      TweenMax.to(".hand4-wrapper", .3, {
-        bottom: '-65%',
+      TweenMax.to(".hand4-wrapper", 0.3, {
+        bottom: "-65%",
         ease: Linear.easeNone,
       }),
-      TweenMax.to(".hand5-wrapper", .3, {
-        bottom: '-70%',
+      TweenMax.to(".hand5-wrapper", 0.3, {
+        bottom: "-70%",
         ease: Linear.easeNone,
       }),
-      TweenMax.to(".hand6-wrapper", .3, {
-        top: '-60%',
+      TweenMax.to(".hand6-wrapper", 0.3, {
+        top: "-60%",
         ease: Linear.easeNone,
       }),
-      TweenMax.to(".scroll-arrow", .3, {
-        bottom: '-80px',
+      TweenMax.to(".scroll-arrow", 0.3, {
+        bottom: "-80px",
         ease: Linear.easeNone,
       }),
     ]);
-  }
-
-  else {
-    console.log('Hand Animation Mobile');
+  } else {
+    console.log("Hand Animation Mobile");
     //Timeinterval for animation delay
+    wipeAnimation.add([TweenMax.to(".dummy", 0.2, {})]);
     wipeAnimation.add([
-      TweenMax.to(".dummy", .2, {})
-    ]);
-    wipeAnimation.add([
-      TweenMax.to(".hand1-wrapper", .5, {
-        left: '-42%',
+      TweenMax.to(".hand1-wrapper", 0.5, {
+        left: "-42%",
         ease: Linear.easeNone,
       }),
-      TweenMax.to(".hand2-wrapper", .5, {
-        right: '-44%',
+      TweenMax.to(".hand2-wrapper", 0.5, {
+        right: "-44%",
         ease: Linear.easeNone,
       }),
     ]);
 
     //Timeinterval for animation delay
+    wipeAnimation.add([TweenMax.to(".dummy", 0.2, {})]);
     wipeAnimation.add([
-      TweenMax.to(".dummy", .2, {})
-    ]);
-    wipeAnimation.add([
-      TweenMax.to(".hand3-wrapper", .5, {
-        top: '-28%',
-        right: '-41%',
+      TweenMax.to(".hand3-wrapper", 0.5, {
+        top: "-28%",
+        right: "-41%",
         ease: Linear.easeNone,
       }),
-      TweenMax.to(".hand4-wrapper", .5, {
-        bottom: '-32%',
-        left: '-26%',
+      TweenMax.to(".hand4-wrapper", 0.5, {
+        bottom: "-32%",
+        left: "-26%",
         ease: Linear.easeNone,
       }),
     ]);
 
     //Timeinterval for animation delay
+    wipeAnimation.add([TweenMax.to(".dummy", 0.2, {})]);
     wipeAnimation.add([
-      TweenMax.to(".dummy", .2, {})
-    ]);
-    wipeAnimation.add([
-      TweenMax.to(".hand5-wrapper", .5, {
-        bottom: '-41%',
-        right: '-31%',
+      TweenMax.to(".hand5-wrapper", 0.5, {
+        bottom: "-41%",
+        right: "-31%",
         ease: Linear.easeNone,
       }),
-      TweenMax.to(".hand6-wrapper", .5, {
-        top: '-34%',
-        left: '-30%',
+      TweenMax.to(".hand6-wrapper", 0.5, {
+        top: "-34%",
+        left: "-30%",
         ease: Linear.easeNone,
       }),
       TweenMax.to(".logo-wrapper--anim", 0.3, {
-        marginTop: '0',
-        opacity: '1',
+        marginTop: "0",
+        opacity: "1",
         ease: Linear.easeNone,
       }),
     ]);
 
     //Timeinterval for animation delay
+    wipeAnimation.add([TweenMax.to(".dummy", 0.2, {})]);
     wipeAnimation.add([
-      TweenMax.to(".dummy", .2, {})
-    ]);
-    wipeAnimation.add([
-      TweenMax.to(".hand1-wrapper", .3, {
-        left: '-60%',
+      TweenMax.to(".hand1-wrapper", 0.3, {
+        left: "-60%",
         ease: Linear.easeNone,
       }),
-      TweenMax.to(".hand2-wrapper", .3, {
-        right: '-60%',
+      TweenMax.to(".hand2-wrapper", 0.3, {
+        right: "-60%",
         ease: Linear.easeNone,
       }),
-      TweenMax.to(".hand3-wrapper", .3, {
-        top: '-60%',
+      TweenMax.to(".hand3-wrapper", 0.3, {
+        top: "-60%",
         ease: Linear.easeNone,
       }),
-      TweenMax.to(".hand4-wrapper", .3, {
-        bottom: '-65%',
+      TweenMax.to(".hand4-wrapper", 0.3, {
+        bottom: "-65%",
         ease: Linear.easeNone,
       }),
-      TweenMax.to(".hand5-wrapper", .3, {
-        bottom: '-70%',
+      TweenMax.to(".hand5-wrapper", 0.3, {
+        bottom: "-70%",
         ease: Linear.easeNone,
       }),
-      TweenMax.to(".hand6-wrapper", .3, {
-        top: '-60%',
+      TweenMax.to(".hand6-wrapper", 0.3, {
+        top: "-60%",
         ease: Linear.easeNone,
       }),
-      TweenMax.to(".scroll-arrow", .3, {
-        bottom: '-80px',
+      TweenMax.to(".scroll-arrow", 0.3, {
+        bottom: "-80px",
         ease: Linear.easeNone,
       }),
     ]);
